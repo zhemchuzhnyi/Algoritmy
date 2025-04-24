55a2de45940e8adc8fd77c55c7e7a961209ed68f
Andrey_O <a_reshkin@inbox.ru>
2025-04-24 12:08:37 +0300

    second Task

--- binary_search/Task_1.py
+++ binary_search/Task_1.py
@@ -1,40 +1,22 @@
-# def binary_search(list, item):
-#     low = 0
-#     high = len(list) - 1
-#     while low <= high:
-#         mid = (low + high) / 2
-#         guess = list[mid]
-#         if guess == item:
-#             high = mid - 1
-#         else:
-#             low =mid + 1
-#         return None
-#
-#     my_list = [1, 3, 5, 7, 9]
-#
-#     print (binary_search(my_list, 9)) # => 1 - нумерация начинается с 0 - вторая ячейка это 1
-#     print (binary_search(my_list, -1)) # => None - в пайтоне означает ничего - признак того что элемент не найден
-
-
 def binary_search(lst, item):
     low = 0
-    high = len(lst) - 1
+    high = len(lst) - 1 # в переменных лоу и хай хранятся границы той части списка в которой выполняется поиск
 
-    while low <= high:
-        mid = (low + high) // 2  # Целочисленное деление
+    while low <= high: # пока эта часть не сократится до 1 элемента
+        mid = (low + high) // 2  # Целочисленное деление - проверяем средний элемент
         guess = lst[mid]
 
         if guess == item:  # Элемент найден
             return mid
-        elif guess < item:  # Ищем в правой части
+        elif guess < item:  # Ищем в правой части - много
             low = mid + 1
-        else:  # Ищем в левой части
+        else:  # Ищем в левой части - мало
             high = mid - 1
 
-    return None  # Элемент не найден
+    return None  # Элемент не найден - значение не существует
 
 # Определяем список и вызываем функцию
 my_list = [1, 3, 5, 7, 9]
 
-print(binary_search(my_list, 3))  # => 4 (индекс элемента 9)
+print(binary_search(my_list, 9))  # => 4 (индекс элемента 9)
 print(binary_search(my_list, -1))  # => None (элемент не найден)
